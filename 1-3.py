b470eb6d86acea1206f9da123ff33ab83ea187cc
bartli555 <barlis3456@gmail.com>
2026-01-10 19:08:54 +0100

    Add files via upload

---
+++ 1-3.py
@@ -0,0 +1,10 @@
+mobile = {
+"OS":"Android",
+"Brand": "Samsung",
+"Model": "Galaxy S23",
+"Storage": "256 GB",
+"Color": "Phantom Black",
+"Battery": "3900 mAh"
+}
+for key, value in mobile.items():
+    print(f"{key} : {value}")
