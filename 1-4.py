cb7688014891a3a2963b3345ee5562ae228113a8
bartli555 <barlis3456@gmail.com>
2026-01-16 12:04:08 +0100

    Update and rename 1-4 do zrobienia.py to 1-4.py

---
+++ 1-4.py
@@ -0,0 +1,22 @@
+person = {
+   "name": "Marek",
+   "surname": "Banach",
+   "age": 25,
+   "hobby": ["swimming", "excursions"],
+   "married": True,
+   "phone": {"landline": "123444321", "mobile": "777888999"}
+}
+
+print(person["name"])
+print(person["hobby"])
+print(person)
+
+person["surname"] = "Nowak"
+person["married"] = False
+person["gender"] = "male"
+person["hobby"].append("rower")
+person["phone"]["work"] = "313131444"
+
+print()
+for key, value in person.items():
+    print(f"{key} : {value}")
